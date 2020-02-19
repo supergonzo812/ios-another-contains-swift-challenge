cbfae6370c58fd6374db1b580f2c8818a520f0bd
Chris <chris.gonzales.82@gmail.com>
2020-02-19 08:46:45 -0800

    Challenge attempt

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -2,6 +2,29 @@ import Foundation
 
 extension String {
     
+    func anotherContains (_ searchString: String) -> Bool{
+//        var result: ComparisonResult
+//        result = searchString.caseInsensitiveCompare(searchString)
+////            searchString.caseInsensitiveCompare(searchString).rawValue
+//        print (result)
+//       return true
+        
+        
+    
+        
+        
+        
+        
+        // Try to loop through base text base on searchString. Should continue loop while strings match. returns true is full string matches, false if not
+        
+//        searchString.contains(<#T##element: Character##Character#>)
+//
+//        for character in searchString{
+//            if character ==
+//        }
+//
+    }
+    
 }
 
 
